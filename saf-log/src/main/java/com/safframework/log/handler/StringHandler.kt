24af39c999c2257a8cfe109d94e808d9502187da
fengzhizi715 <fengzhizi715@126.com>
2017-11-27 19:13:17 +0800

    重构代码

--- saf-log/src/main/java/com/safframework/log/handler/StringHandler.kt
+++ saf-log/src/main/java/com/safframework/log/handler/StringHandler.kt
@@ -1,12 +1,16 @@
 package com.safframework.log.handler
 
 import com.safframework.log.L
-import com.safframework.log.parser.StringParser
+import com.safframework.log.LoggerPrinter
+import com.safframework.log.parser.Parser
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 /**
  * Created by tony on 2017/11/27.
  */
-class StringHandler():BaseHandler() {
+class StringHandler():BaseHandler(), Parser<String> {
 
     override fun handle(obj: Any): Boolean {
 
@@ -14,12 +18,32 @@ class StringHandler():BaseHandler() {
 
             var json = obj.trim { it <= ' ' }
             val s = L.getMethodNames()
-            val parser = StringParser()
-            println(String.format(s, parser.parseString(json)))
+            println(String.format(s, parseString(json)))
             return true
         }
 
         return false
+    }
+
+    override fun parseString(json: String): String {
+        var message: String = ""
+
+        try {
+            if (json.startsWith("{")) {
+                val jsonObject = JSONObject(json)
+                message = jsonObject.toString(LoggerPrinter.JSON_INDENT)
+                message = message.replace("\n".toRegex(), "\n║ ")
+            } else if (json.startsWith("[")) {
+                val jsonArray = JSONArray(json)
+                message = jsonArray.toString(LoggerPrinter.JSON_INDENT)
+                message = message.replace("\n".toRegex(), "\n║ ")
+            }
+        } catch (e: JSONException) {
+            L.e("Invalid Json: " + json)
+            message = ""
+        }
+
+        return message
     }
 
 }
--- saf-log/src/main/java/com/safframework/log/handler/UriHandler.kt
+++ saf-log/src/main/java/com/safframework/log/handler/UriHandler.kt
@@ -2,24 +2,43 @@ package com.safframework.log.handler
 
 import android.net.Uri
 import com.safframework.log.L
-import com.safframework.log.parser.UriParser
+import com.safframework.log.LoggerPrinter
+import com.safframework.log.parser.Parser
+import org.json.JSONObject
 
 /**
  * Created by tony on 2017/11/27.
  */
-class UriHandler():BaseHandler() {
+class UriHandler():BaseHandler(), Parser<Uri> {
 
     override fun handle(obj: Any): Boolean {
 
         if (obj is Uri) {
 
             val s = L.getMethodNames()
-            val parser = UriParser()
-            println(String.format(s, parser.parseString(obj)))
+            println(String.format(s, parseString(obj)))
             return true
         }
 
         return false
+    }
+
+    override fun parseString(uri: Uri): String {
+
+        var msg = uri.javaClass.toString() + LoggerPrinter.BR + "║ "
+
+        val jsonObject = JSONObject()
+        jsonObject.put("Scheme", uri.scheme)
+        jsonObject.put("Host", uri.host)
+        jsonObject.put("Port", uri.port)
+        jsonObject.put("Path", uri.path)
+        jsonObject.put("Query", uri.query)
+        jsonObject.put("Fragment", uri.fragment)
+
+        var message = jsonObject.toString(LoggerPrinter.JSON_INDENT)
+        message = message.replace("\n".toRegex(), "\n║ ")
+
+        return msg + message
     }
 
 }
