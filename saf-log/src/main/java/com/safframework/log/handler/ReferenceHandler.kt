d537c5ddf0a5ff039d4ec9cdb9a4ea1f83a4a75e
fengzhizi715 <fengzhizi715@126.com>
2017-11-27 19:22:29 +0800

    重构代码

--- saf-log/src/main/java/com/safframework/log/handler/ReferenceHandler.kt
+++ saf-log/src/main/java/com/safframework/log/handler/ReferenceHandler.kt
@@ -1,23 +1,47 @@
 package com.safframework.log.handler
 
+import com.alibaba.fastjson.JSON
 import com.safframework.log.L
-import com.safframework.log.parser.ReferenceParser
+import com.safframework.log.LoggerPrinter
+import com.safframework.log.parser.Parser
+import com.safframework.log.utils.Utils
+import org.json.JSONObject
 import java.lang.ref.Reference
 
 /**
  * Created by tony on 2017/11/27.
  */
-class ReferenceHandler():BaseHandler() {
+class ReferenceHandler():BaseHandler(), Parser<Reference<*>> {
 
     override fun handle(obj: Any): Boolean {
 
         if (obj is Reference<*>) {
             val s = L.getMethodNames()
-            val parser = ReferenceParser()
-            println(String.format(s, parser.parseString(obj)))
+            println(String.format(s, parseString(obj)))
             return true
         }
 
         return false
+    }
+
+    override fun parseString(reference: Reference<*>): String {
+        val actual = reference.get()
+
+        var msg = reference.javaClass.canonicalName + "<" + actual?.javaClass?.simpleName + ">"+ LoggerPrinter.BR + "║ "
+
+        val isPrimitiveType = Utils.isPrimitiveType(actual)
+
+        if (isPrimitiveType) {
+
+            msg += "{" + actual + "}"
+        } else {
+
+            val jsonObject = JSONObject(JSON.toJSONString(actual))
+            var message = jsonObject.toString(LoggerPrinter.JSON_INDENT)
+            message = message.replace("\n".toRegex(), "\n║ ")
+            msg += message
+        }
+
+        return msg
     }
 }
--- saf-log/src/main/java/com/safframework/log/handler/ThrowableHandler.kt
+++ saf-log/src/main/java/com/safframework/log/handler/ThrowableHandler.kt
@@ -1,24 +1,36 @@
 package com.safframework.log.handler
 
 import com.safframework.log.L
-import com.safframework.log.parser.ThrowableParser
+import com.safframework.log.parser.Parser
+import java.io.PrintWriter
+import java.io.StringWriter
 
 /**
  * Created by tony on 2017/11/27.
  */
-class ThrowableHandler():BaseHandler() {
+class ThrowableHandler():BaseHandler(), Parser<Throwable> {
 
     override fun handle(obj: Any): Boolean {
 
         if (obj is Throwable) {
 
             val s = L.getMethodNames()
-            val parser = ThrowableParser()
-            System.err.println(String.format(s, parser.parseString(obj)))
+            System.err.println(String.format(s, parseString(obj)))
             return true
         }
 
         return false
+    }
+
+    override fun parseString(throwable: Throwable): String {
+        val sw = StringWriter(256)
+        val pw = PrintWriter(sw, false)
+        throwable.printStackTrace(pw)
+        pw.flush()
+        var message = sw.toString()
+        message = message.replace("\n".toRegex(), "\n║ ")
+
+        return message
     }
 
 }
