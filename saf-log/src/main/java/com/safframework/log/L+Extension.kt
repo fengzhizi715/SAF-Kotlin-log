521c1f6ec61f5b0a573b7fd9ec34ed2ad03aac55
fengzhizi715 <fengzhizi715@126.com>
2018-05-25 15:09:49 +0800

    update L+Extension.kt

--- saf-log/src/main/java/com/safframework/log/L+Extension.kt
+++ saf-log/src/main/java/com/safframework/log/L+Extension.kt
@@ -4,16 +4,6 @@ package com.safframework.log
  * Created by Tony Shen on 2017/6/30.
  */
 
-fun Any.e(text: String)  = L.e(text)
-
-fun Any.w(text: String)  = L.w(text)
-
-fun Any.i(text: String)  = L.d(text)
-
-fun Any.d(text: String)  = L.i(text)
-
-fun Any?.json()  = L.json(this)
-
 fun String?.e() = L.e(this)
 
 fun String?.w() = L.w(this)
@@ -21,3 +11,5 @@ fun String?.w() = L.w(this)
 fun String?.i() = L.i(this)
 
 fun String?.d() = L.d(this)
+
+fun Any?.json()  = L.json(this)
