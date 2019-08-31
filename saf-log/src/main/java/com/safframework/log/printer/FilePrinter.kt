3fe9b3b170f9594d0151d9681558c5876872601b
fengzhizi715 <fengzhizi715@126.com>
2019-08-31 21:48:58 +0800

    update FilePrinter

--- saf-log/src/main/java/com/safframework/log/printer/FilePrinter.kt
+++ saf-log/src/main/java/com/safframework/log/printer/FilePrinter.kt
@@ -5,6 +5,10 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.launch
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
 
 
 /**
@@ -18,12 +22,13 @@ import kotlinx.coroutines.launch
 object FilePrinter:Printer{
 
     private val channel = Channel<LogItem>()
+    private var folderPath:String?=null
 
     init {
         GlobalScope.launch {
             channel.consumeEach {
 
-                doPrintln(it.timeMillis,it.level,it.tag,it.msg)
+                doWrite(it)
             }
         }
     }
@@ -37,10 +42,83 @@ object FilePrinter:Printer{
         }
     }
 
-    private fun doPrintln(timeMillis: Long, logLevel: LogLevel, tag: String, msg: String) {
+    private fun doWrite(logItem:LogItem) {
+
 
     }
 
     private class LogItem internal constructor(internal var timeMillis: Long, internal var level: LogLevel, internal var tag: String, internal var msg: String)
+
+
+    private class Writer {
+
+        var lastFileName: String? = null
+            private set
+
+        var file: File? = null
+            private set
+
+        lateinit var bufferedWriter: BufferedWriter
+
+        val isOpened: Boolean
+            get() = bufferedWriter != null
+
+         fun open(newFileName: String): Boolean {
+            lastFileName = newFileName
+            file = File(folderPath, newFileName)
+
+            if (!file!!.exists()) {
+                try {
+                    val parent = file!!.getParentFile()
+                    if (!parent.exists()) {
+                        parent.mkdirs()
+                    }
+                    file!!.createNewFile()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                    lastFileName = null
+                    file = null
+                    return false
+                }
+
+            }
+
+            try {
+                bufferedWriter = BufferedWriter(FileWriter(file, true))
+            } catch (e: Exception) {
+                e.printStackTrace()
+                lastFileName = null
+                file = null
+                return false
+            }
+
+            return true
+        }
+
+        fun close(): Boolean {
+            if (bufferedWriter != null) {
+                try {
+                    bufferedWriter.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                    return false
+                } finally {
+                    lastFileName = null
+                    file = null
+                }
+            }
+            return true
+        }
+
+        fun appendLog(log: String) {
+            try {
+                bufferedWriter.write(log)
+                bufferedWriter.newLine()
+                bufferedWriter.flush()
+            } catch (e: IOException) {
+            }
+
+        }
+    }
 
 }
