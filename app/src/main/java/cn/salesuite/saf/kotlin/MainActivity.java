98b5b95feca9177ad03c652e55b7cfdf7e6ced67
fengzhizi715 <fengzhizi715@126.com>
2019-09-05 22:43:18 +0800

    update build.gradle

--- app/src/main/java/cn/salesuite/saf/kotlin/MainActivity.java
+++ app/src/main/java/cn/salesuite/saf/kotlin/MainActivity.java
@@ -1,12 +1,21 @@
 package cn.salesuite.saf.kotlin;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import com.safframework.log.L;
 import com.safframework.log.handler.BaseHandler;
+import com.safframework.log.printer.FilePrinter;
+import com.safframework.log.printer.file.DateFileNameGenerator;
+import com.safframework.log.printer.file.FileBuilder;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
@@ -31,6 +40,19 @@ public class MainActivity extends Activity{
 //        L.w("test","haha");
 //        L.d("test","haha");
 //        L.e("test","haha");
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            //有权限
+            FilePrinter filePrinter = new FileBuilder("/storage/emulated/0").fileNameGenerator(new DateFileNameGenerator()).build();
+            L.printer(filePrinter);
+        } else {
+            //没权限
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        }
+
+
         L.i("111321frehtyjuyikuloil'0[xwrgrtehcytbk8ynfggrgr4hytj");
 
         User u = new User();
@@ -74,5 +96,15 @@ public class MainActivity extends Activity{
 //        L.addCustomerHandler(new UserHandler());
 //        L.json(u);
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+
+        if (requestCode==0) {
+
+            FilePrinter filePrinter = new FileBuilder("/storage/emulated/0").fileNameGenerator(new DateFileNameGenerator()).build();
+            L.printer(filePrinter);
+        }
     }
 }
