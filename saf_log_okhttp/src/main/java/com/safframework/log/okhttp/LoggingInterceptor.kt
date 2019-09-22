4d5f249c0a65d8e7deb8f01f758a8e37cf43574f
fengzhizi715 <fengzhizi715@126.com>
2019-09-22 14:34:19 +0800

    update LoggingInterceptor.kt

--- saf_log_okhttp/src/main/java/com/safframework/log/okhttp/LoggingInterceptor.kt
+++ saf_log_okhttp/src/main/java/com/safframework/log/okhttp/LoggingInterceptor.kt
@@ -1,8 +1,13 @@
 package com.safframework.log.okhttp
 
 import com.safframework.log.L
+import com.safframework.log.LoggerPrinter
 import okhttp3.Interceptor
 import okhttp3.Response
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import java.nio.charset.Charset
 
 /**
  *
@@ -14,17 +19,71 @@ import okhttp3.Response
  */
 class LoggingInterceptor: Interceptor {
 
+    companion object {
+
+        private const val JSON_INDENT = 3
+        private const val MAX_STRING_LENGTH = 4000
+    }
+
+
     override fun intercept(chain: Interceptor.Chain): Response {
 
         var request = chain.request()
+        val header = request.headers.toString()
 
-        L.json(request)
+        val requestLog = " URL: " + request.url  + LoggerPrinter.BR +
+                " Method: @" + request.method  + LoggerPrinter.BR +
+                if (header.isNullOrEmpty()) "" else " Headers: " + header + LoggerPrinter.BR
+
+        L.i(requestLog)
 
         val response = chain.proceed(request)
 
-        L.json(response)
+        val responseBody = response.body
+        val contentType = responseBody!!.contentType()
+
+        var subtype: String? = null
+
+        if (contentType != null) {
+            subtype = contentType.subtype
+        }
+
+        if (subtypeIsNotFile(subtype)) {
+
+            val source = responseBody.source()
+            source.request(Long.MAX_VALUE)
+            val buffer = source.buffer()
+
+            val bodyString = getJsonString(buffer.clone().readString(Charset.forName("UTF-8")))
+
+            L.i(bodyString)
+        }
 
         return response
     }
 
+    private fun subtypeIsNotFile(subtype: String?): Boolean = subtype != null && (subtype.contains("json")
+            || subtype.contains("xml")
+            || subtype.contains("plain")
+            || subtype.contains("html"))
+
+    private fun getJsonString(msg: String): String {
+
+        var message: String
+        try {
+            if (msg.startsWith("{")) {
+                val jsonObject = JSONObject(msg)
+                message = jsonObject.toString(JSON_INDENT)
+            } else if (msg.startsWith("[")) {
+                val jsonArray = JSONArray(msg)
+                message = jsonArray.toString(JSON_INDENT)
+            } else {
+                message = msg
+            }
+        } catch (e: JSONException) {
+            message = msg
+        }
+
+        return message
+    }
 }
