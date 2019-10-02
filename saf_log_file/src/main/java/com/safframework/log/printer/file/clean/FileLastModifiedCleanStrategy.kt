076fb1559e8157979462e81a32d86f27e8ebd2ba
fengzhizi715 <fengzhizi715@126.com>
2019-10-02 21:35:06 +0800

    update FileLastModifiedCleanStrategy

--- saf_log_file/src/main/java/com/safframework/log/printer/file/clean/FileLastModifiedCleanStrategy.kt
+++ saf_log_file/src/main/java/com/safframework/log/printer/file/clean/FileLastModifiedCleanStrategy.kt
@@ -10,10 +10,10 @@ import java.io.File
  * @date: 2019-10-02 18:53
  * @version: V2.1 根据设置文件可以保存的最大时间，来实现文件的清除策略
  */
-class FileLastModifiedCleanStrategy(private val maxTimeMillis: Long) : CleanStrategy {
+class FileLastModifiedCleanStrategy(val maxTimeMillis: Long) : CleanStrategy {
 
     override fun shouldClean(file: File): Boolean {
-        
+
         val currentTimeMillis = System.currentTimeMillis()
         val lastModified = file.lastModified()
         return currentTimeMillis - lastModified > maxTimeMillis
