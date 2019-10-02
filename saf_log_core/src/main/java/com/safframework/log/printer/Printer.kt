90d34e6f221ee73c5e55090f5ccbfe1a99c1525c
fengzhizi715 <fengzhizi715@126.com>
2019-10-02 17:21:33 +0800

    增加注释

--- saf_log_core/src/main/java/com/safframework/log/printer/Printer.kt
+++ saf_log_core/src/main/java/com/safframework/log/printer/Printer.kt
@@ -9,11 +9,14 @@ import com.safframework.log.formatter.Formatter
  *          com.safframework.log.printer.Printer
  * @author: Tony Shen
  * @date: 2019-08-15 00:26
- * @version: V1.0 打印日志，支持打印日志输出到 Console、File
+ * @version: V1.0 打印日志，支持打印日志输出到 Console、File 等等
  */
 interface Printer {
 
-    val formatter: Formatter
+    val formatter: Formatter // 用于格式化日志
 
+    /**
+     * 打印日志
+     */
     fun printLog(logLevel: LogLevel, tag: String, msg: String)
 }
