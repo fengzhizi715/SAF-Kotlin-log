da471bb4577187cf38f1380ecfb990da0ece89e9
fengzhizi715 <fengzhizi715@126.com>
2019-10-03 13:24:29 +0800

    update StringHandler

--- saf_log_core/src/main/java/com/safframework/log/handler/StringHandler.kt
+++ saf_log_core/src/main/java/com/safframework/log/handler/StringHandler.kt
@@ -5,6 +5,7 @@ import com.safframework.log.LogLevel
 import com.safframework.log.formatter.Formatter
 import com.safframework.log.logTag
 import com.safframework.log.parser.Parser
+import com.safframework.log.printer.Printer
 import com.safframework.log.utils.formatJSON
 import org.json.JSONArray
 import org.json.JSONException
@@ -15,6 +16,10 @@ import org.json.JSONObject
  */
 class StringHandler:BaseHandler(), Parser<String> {
 
+    companion object {
+        private const val MAX_STRING_LENGTH = 4000
+    }
+
     override fun handle(obj: Any): Boolean {
 
         if (obj is String) {
@@ -23,7 +28,8 @@ class StringHandler:BaseHandler(), Parser<String> {
 
             L.printers().map {
                 val s = L.getMethodNames(it.formatter)
-                it.printLog(LogLevel.INFO, this.logTag(),String.format(s, parseString(json,it.formatter)))
+                val logString = String.format(s, parseString(json,it.formatter))
+                printLongLog(LogLevel.INFO,this.logTag(),logString,it)
             }
             return true
         }
@@ -31,8 +37,34 @@ class StringHandler:BaseHandler(), Parser<String> {
         return false
     }
 
+    /**
+     * 打印超过4000行的日志
+     */
+    private fun printLongLog(logLevel: LogLevel,tag: String, logString: String, printer:Printer) {
+
+        if (logString.length > MAX_STRING_LENGTH) {
+
+            var i = 0
+
+            while (i < logString.length) {
+
+                if (i + MAX_STRING_LENGTH < logString.length) {
+                    if (i==0) {
+                        printer.printLog(logLevel, tag, logString.substring(i, i + MAX_STRING_LENGTH))
+                    } else {
+                        printer.printLog(logLevel, "", logString.substring(i, i + MAX_STRING_LENGTH))
+                    }
+                } else
+                    printer.printLog(logLevel, "", logString.substring(i, logString.length))
+
+                i += MAX_STRING_LENGTH
+            }
+        } else
+            printer.printLog(logLevel, tag, logString)
+    }
+
     override fun parseString(json: String,formatter: Formatter): String {
-        var message: String = ""
+        var message = ""
 
         try {
             if (json.startsWith("{")) {
