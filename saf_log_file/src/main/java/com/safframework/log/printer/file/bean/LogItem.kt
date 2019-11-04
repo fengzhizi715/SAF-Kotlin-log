f609b4c8251253e3919ce7c36e24ef21ae605e6d
fengzhizi715 <fengzhizi715@126.com>
2019-11-04 20:51:41 +0800

    update LogItem

--- saf_log_file/src/main/java/com/safframework/log/printer/file/bean/LogItem.kt
+++ saf_log_file/src/main/java/com/safframework/log/printer/file/bean/LogItem.kt
@@ -10,4 +10,4 @@ import com.safframework.log.LogLevel
  * @date: 2019-11-04 17:09
  * @version: V2.0 每次写入文件的内容，记录了当前时间、LogLevel、tag、msg
  */
-class LogItem constructor(internal var timeMillis: Long, internal var level: LogLevel, internal var tag: String, internal var msg: String)
+class LogItem(internal var timeMillis: Long, internal var level: LogLevel, internal var tag: String, internal var msg: String)
