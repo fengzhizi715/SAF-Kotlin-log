8f4068ce39f9d5831020281df8cbdee4fa28200b
fengzhizi715 <fengzhizi715@126.com>
2019-12-08 00:38:09 +0800

    add Converter

---
+++ saf_log_core/src/main/java/com/safframework/log/converter/Converter.kt
@@ -0,0 +1,30 @@
+package com.safframework.log.converter
+
+import java.lang.reflect.Type
+
+/**
+ *
+ * @FileName:
+ *          com.safframework.log.converter.Converter
+ * @author: Tony Shen
+ * @date: 2019-12-08 00:36
+ * @version: V2.3 <描述当前版本功能>
+ */
+interface Converter {
+
+    /**
+     * 将字符串转换成type类型的对象
+     * @param json
+     * @param type
+     * @param <T>
+     * @return
+    </T> */
+    fun <T> fromJson(json: String?, type: Type?): T
+
+    /**
+     * 将对象序列化成字符串对象
+     * @param data
+     * @return
+     */
+    fun toJson(data: Any?): String?
+}
