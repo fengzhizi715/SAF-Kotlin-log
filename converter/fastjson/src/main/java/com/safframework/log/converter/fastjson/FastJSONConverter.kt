4138ca46bf757e3a52d0b42d516ba8a187f6eaa8
fengzhizi715 <fengzhizi715@126.com>
2019-12-09 13:03:31 +0800

    add FastJSONConverter

---
+++ converter/fastjson/src/main/java/com/safframework/log/converter/fastjson/FastJSONConverter.kt
@@ -0,0 +1,27 @@
+package com.safframework.log.converter.fastjson
+
+import com.alibaba.fastjson.JSON
+import com.safframework.log.converter.Converter
+import java.lang.reflect.Type
+
+/**
+ *
+ * @FileName:
+ *          com.safframework.log.converter.FastJSONConverter
+ * @author: Tony Shen
+ * @date: 2019-12-09 01:36
+ * @version: V2.4 <描述当前版本功能>
+ */
+class FastJSONConverter : Converter {
+
+    override fun <T> fromJson(json: String, type: Type): T {
+
+        return JSON.parseObject(json, type)
+    }
+
+    override fun toJson(data: Any): String {
+
+        return JSON.toJSONString(data)
+    }
+
+}
