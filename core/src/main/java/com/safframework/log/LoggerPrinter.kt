f2ff6ad1d631833dd71cad7caf3609c0c50818e4
fengzhizi715 <fengzhizi715@126.com>
2019-12-10 22:44:33 +0800

    update LoggerPrinter

--- core/src/main/java/com/safframework/log/LoggerPrinter.kt
+++ core/src/main/java/com/safframework/log/LoggerPrinter.kt
@@ -15,20 +15,20 @@ object LoggerPrinter {
     /**
      * Drawing toolbox
      */
-    private val TOP_LEFT_CORNER    = '╔'
-    private val BOTTOM_LEFT_CORNER = '╚'
-    private val MIDDLE_CORNER      = '╟'
-    private val DOUBLE_DIVIDER     = "═════════════════════════════════════════════════"
-    private val SINGLE_DIVIDER     = "─────────────────────────────────────────────────"
+    private const val TOP_LEFT_CORNER    = '╔'
+    private const val BOTTOM_LEFT_CORNER = '╚'
+    private const val MIDDLE_CORNER      = '╟'
+    private const val DOUBLE_DIVIDER     = "═════════════════════════════════════════════════"
+    private const val SINGLE_DIVIDER     = "─────────────────────────────────────────────────"
 
-    val TOP_BORDER          = TOP_LEFT_CORNER + DOUBLE_DIVIDER + DOUBLE_DIVIDER
-    val BOTTOM_BORDER       = BOTTOM_LEFT_CORNER + DOUBLE_DIVIDER + DOUBLE_DIVIDER
-    val MIDDLE_BORDER       = MIDDLE_CORNER + SINGLE_DIVIDER + SINGLE_DIVIDER
+    val TOP_BORDER               = TOP_LEFT_CORNER + DOUBLE_DIVIDER + DOUBLE_DIVIDER
+    val BOTTOM_BORDER            = BOTTOM_LEFT_CORNER + DOUBLE_DIVIDER + DOUBLE_DIVIDER
+    val MIDDLE_BORDER            = MIDDLE_CORNER + SINGLE_DIVIDER + SINGLE_DIVIDER
 
-    val BR                 = System.getProperty("line.separator")     // 换行符
-    val HORIZONTAL_DOUBLE_LINE     = "║ "
-    val BLANK                      = " "
-    val COMMA                      = ","
+    val BR                       = System.getProperty("line.separator")     // 换行符
+    const val HORIZONTAL_DOUBLE_LINE     = "║ "
+    const val BLANK                      = " "
+    const val COMMA                      = ","
 
     fun getStackOffset(trace: Array<StackTraceElement>): Int {
         var i = MIN_STACK_OFFSET
