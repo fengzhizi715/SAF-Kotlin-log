cbe5099f11e135f1ea86faa312dba1d877435172
fengzhizi715 <fengzhizi715@126.com>
2019-12-11 17:24:59 +0800

    add LWrapper.kt

---
+++ core/src/main/java/com/safframework/log/LWrapper.kt
@@ -0,0 +1,50 @@
+package com.safframework.log
+
+import com.safframework.log.converter.Converter
+
+/**
+ *
+ * @FileName:
+ *          com.safframework.log.LWrapper
+ * @author: Tony Shen
+ * @date: 2019-12-11 17:10
+ * @version: V2.4 <描述当前版本功能>
+ */
+class LWrapper {
+
+    var logLevel:LogLevel = LogLevel.DEBUG
+
+    var header:String? = null
+
+    var tag:String = "SAF_L"
+
+    var converter: Converter?=null
+}
+
+fun config(init: LWrapper.() -> Unit):L {
+
+    val wrap = LWrapper()
+
+    wrap.init()
+
+    return configL(wrap)
+}
+
+internal fun configL(wrap:LWrapper):L {
+
+    L.logLevel = wrap.logLevel
+
+    wrap.header?.let {
+        L.header(it)
+    }
+
+    wrap.tag?.let {
+        L.init(it)
+    }
+
+    wrap.converter?.let {
+        L.converter(it)
+    }
+
+    return L
+}
