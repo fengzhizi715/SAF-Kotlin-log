1d92b2dd67697e38ec16513ba6add9a59cc47da0
fengzhizi715 <fengzhizi715@126.com>
2019-12-14 11:34:51 +0800

    update

--- file/src/main/java/com/safframework/log/printer/utils/Utils.kt
+++ file/src/main/java/com/safframework/log/printer/utils/Utils.kt
@@ -12,11 +12,9 @@ import java.util.*
  * @version: V1.0 <描述当前版本功能>
  */
 
-fun now(): String {
-    return if (android.os.Build.VERSION.SDK_INT >= 24){
-        SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(Date())
-    }else{
-        var tms = Calendar.getInstance()
-        tms.get(Calendar.YEAR).toString() + "-" + tms.get(Calendar.MONTH).toString() + "-" + tms.get(Calendar.DAY_OF_MONTH).toString() + " " + tms.get(Calendar.HOUR_OF_DAY).toString() + ":" + tms.get(Calendar.MINUTE).toString() +":" + tms.get(Calendar.SECOND).toString() +"." + tms.get(Calendar.MILLISECOND).toString()
-    }
+fun now(): String = if (android.os.Build.VERSION.SDK_INT >= 24) {
+    SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(Date())
+} else {
+    var tms = Calendar.getInstance()
+    tms.get(Calendar.YEAR).toString() + "-" + tms.get(Calendar.MONTH).toString() + "-" + tms.get(Calendar.DAY_OF_MONTH).toString() + " " + tms.get(Calendar.HOUR_OF_DAY).toString() + ":" + tms.get(Calendar.MINUTE).toString() + ":" + tms.get(Calendar.SECOND).toString() + "." + tms.get(Calendar.MILLISECOND).toString()
 }
