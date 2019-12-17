d08cdc914a09ba67cdabd581742d36f9c729b4b5
fengzhizi715 <fengzhizi715@126.com>
2019-12-18 01:01:36 +0800

    update build.gradle

--- file/src/main/java/com/safframework/log/printer/utils/Utils.kt
+++ file/src/main/java/com/safframework/log/printer/utils/Utils.kt
@@ -16,5 +16,5 @@ fun now(): String = if (android.os.Build.VERSION.SDK_INT >= 24) {
     SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(Date())
 } else {
     var tms = Calendar.getInstance()
-    tms.get(Calendar.YEAR).toString() + "-" + tms.get(Calendar.MONTH).toString() + "-" + tms.get(Calendar.DAY_OF_MONTH).toString() + " " + tms.get(Calendar.HOUR_OF_DAY).toString() + ":" + tms.get(Calendar.MINUTE).toString() + ":" + tms.get(Calendar.SECOND).toString() + "." + tms.get(Calendar.MILLISECOND).toString()
+    tms.get(Calendar.YEAR).toString() + "-" + tms.get(Calendar.MONTH).toString() + "-" + (tms.get(Calendar.DAY_OF_MONTH)+1).toString() + " " + tms.get(Calendar.HOUR_OF_DAY).toString() + ":" + tms.get(Calendar.MINUTE).toString() + ":" + tms.get(Calendar.SECOND).toString() + "." + tms.get(Calendar.MILLISECOND).toString()
 }
