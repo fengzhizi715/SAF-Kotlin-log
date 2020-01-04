b5f00f058ac2c5c70f5fe2a15629693766547656
fengzhizi715 <fengzhizi715@126.com>
2020-01-04 11:09:08 +0800

    update Utils

--- file/src/main/java/com/safframework/log/printer/utils/Utils.kt
+++ file/src/main/java/com/safframework/log/printer/utils/Utils.kt
@@ -11,10 +11,6 @@ import java.util.*
  * @date: 2019-12-13 16:28
  * @version: V1.0 <描述当前版本功能>
  */
+val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
 
-fun now(): String = if (android.os.Build.VERSION.SDK_INT >= 24) {
-    SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(Date())
-} else {
-    var tms = Calendar.getInstance()
-    tms.get(Calendar.YEAR).toString() + "-" + tms.get(Calendar.MONTH).toString() + "-" + (tms.get(Calendar.DAY_OF_MONTH)+1).toString() + " " + tms.get(Calendar.HOUR_OF_DAY).toString() + ":" + tms.get(Calendar.MINUTE).toString() + ":" + tms.get(Calendar.SECOND).toString() + "." + tms.get(Calendar.MILLISECOND).toString()
-}
+fun now() = sdf.format(Date())
