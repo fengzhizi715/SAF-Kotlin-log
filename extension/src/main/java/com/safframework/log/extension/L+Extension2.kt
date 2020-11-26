57d99f69df78274e6ebcf8babe579abf77aa8787
fengzhizi715 <fengzhizi715@126.com>
2020-11-26 22:02:07 +0800

    update L.bytes()

--- extension/src/main/java/com/safframework/log/extension/L+Extension2.kt
+++ extension/src/main/java/com/safframework/log/extension/L+Extension2.kt
@@ -11,8 +11,27 @@ import com.safframework.log.L
  * @date: 2020-11-26 21:25
  * @version: V1.0 <描述当前版本功能>
  */
-fun L.bytes(bytes: ByteArray) {
+fun L.bytes(bytes: ByteArray, separator: CharSequence = "-") {
+    val hexString = ByteBufferBytes.create(bytes).toHexString()
+    if (separator.isNotEmpty()) {
+        var m: Int = hexString.length / 2
+        if (m * 2 < hexString.length) {
+            m++
+        }
+        val strs = arrayOfNulls<String>(m)
+        var j = 0
+        for (i in 0 until hexString.length) {
+            if (i % 2 == 0) { //每隔两个
+                strs[j] = "" + hexString[i]
+            } else {
+                strs[j] = strs[j] + hexString[i] //将字符加上du两个空格
+                j++
+            }
+        }
 
-    val byteString = ByteBufferBytes.create(bytes).toHexString()
-    L.i(byteString)
+        val newHexString = strs.joinToString ("-")
+        L.i(newHexString)
+    } else {
+        L.i(hexString)
+    }
 }
