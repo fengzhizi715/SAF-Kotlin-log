71dddac31b6c7c2609b884f813b256bf84629bdd
fengzhizi715 <fengzhizi715@126.com>
2020-11-26 21:49:45 +0800

    update

---
+++ extension/src/main/java/com/safframework/log/extension/L+Extension2.kt
@@ -0,0 +1,18 @@
+package com.safframework.log.extension
+
+import com.safframework.bytekit.bytes.ByteBufferBytes
+import com.safframework.log.L
+
+/**
+ *
+ * @FileName:
+ *          com.safframework.log.extension.`L+Extension2`
+ * @author: Tony Shen
+ * @date: 2020-11-26 21:25
+ * @version: V1.0 <描述当前版本功能>
+ */
+fun L.bytes(bytes: ByteArray) {
+
+    val byteString = ByteBufferBytes.create(bytes).toHexString()
+    L.i(byteString)
+}
